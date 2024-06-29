de472f06e20015e7a282809d3d6bfe28aa1a04d8
Stan <stephenppercival@gmail.com>
2024-06-29 23:19:58 +0100

    use max id rather than magic number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import { max } from "drizzle-orm";
 import { db } from "~/server/db";
+import { boardGame } from "~/server/db/schema";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
+  const result = await db.select({ maxId: max(boardGame.id) }).from(boardGame);
+  const maxId = result?.[0]?.maxId ?? 0;
   let randomBoardGame: Awaited<ReturnType<typeof GetBoardGameFromRandomId>>;
 
   while (!randomBoardGame) {
     // bit of a hack just to test db connection
-    randomBoardGame = await GetBoardGameFromRandomId();
+    randomBoardGame = await GetBoardGameFromRandomId(maxId);
   }
 
   return (
@@ -16,8 +20,7 @@ export default async function HomePage() {
   );
 }
 
-async function GetBoardGameFromRandomId() {
-  const maxId = 295770;
+async function GetBoardGameFromRandomId(maxId: number) {
   const randomId = Math.floor(Math.random() * (maxId + 1));
   const game = await db.query.boardGame.findFirst({
     where: (bg, { eq }) => eq(bg.id, randomId),
