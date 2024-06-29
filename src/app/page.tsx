af287e557bcfb050b2fa7381563c3e1d85321cec
Stan <stephenppercival@gmail.com>
2024-06-29 18:48:12 +0100

    tidy random boardgame logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 import { db } from "~/server/db";
-
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  let randomBoardGame:
-    | {
-        id: number;
-        name: string;
-        yearPublished: number;
-        createdAt: Date;
-        updatedAt: Date | null;
-      }
-    | undefined = undefined;
+  let randomBoardGame = await GetBoardGameFromRandomId();
 
   while (!randomBoardGame) {
     // bit of a hack just to test db connection
-    const maxId = 295770;
-    const randomId = Math.floor(Math.random() * (maxId + 1));
-    randomBoardGame = await db.query.boardGame.findFirst({
-      where: (bg, { eq }) => eq(bg.id, randomId),
-    });
+    randomBoardGame = await GetBoardGameFromRandomId();
   }
 
   return (
@@ -27,4 +14,14 @@ export default async function HomePage() {
       {randomBoardGame?.name ?? "No game found"}
     </main>
   );
+}
+
+async function GetBoardGameFromRandomId() {
+  const maxId = 295770;
+  const randomId = Math.floor(Math.random() * (maxId + 1));
+  const game = await db.query.boardGame.findFirst({
+    where: (bg, { eq }) => eq(bg.id, randomId),
+  });
+
+  return game;
 }
