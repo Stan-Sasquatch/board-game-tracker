7bf51d913548bc484e64df3b9883f720839b730c
Stan <stephenppercival@gmail.com>
2024-07-01 21:53:40 +0100

    basic routing

---
+++ src/app/_components/NavBar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function NavBar() {
+  const pathName = usePathname();
+
+  const links = [
+    { href: "/", title: "Home" },
+    { href: "/search", title: "All Games" },
+    { href: "/collection", title: "My Collection" },
+  ];
+
+  return (
+    <div className="flex flex-row gap-5">
+      {links.map((l) => (
+        <div key={l.href}>
+          <Link
+            className={`hover:text-gray-600 ${
+              pathName == l.href ? "underline underline-offset-4" : ""
+            }`}
+            href={l.href}
+          >
+            {l.title}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,10 +1,11 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { NavBar } from "./NavBar";
 
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>All Games</div>
-      <div className="flex flex-row">
+      <NavBar />
+      <div className="flex flex-row justify-between">
         <SignedOut>
           <SignInButton />
         </SignedOut>
