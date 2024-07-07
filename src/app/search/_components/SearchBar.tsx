22c6352dbb3031e491362c135888c2955ec2f838
Stan <stephenppercival@gmail.com>
2024-07-07 18:07:34 +0100

    fix controller vs uncontrolled issue

--- src/app/search/_components/SearchBar.tsx
+++ src/app/search/_components/SearchBar.tsx
@@ -3,22 +3,24 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { boardGameNameKey } from "../models";
 import { useDebouncedCallback } from "use-debounce";
 import { Command, CommandInput } from "~/components/ui/command";
+import { type ChangeEventHandler } from "react";
 
 export function SearchBar({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
 
-  const onChange = useDebouncedCallback((value: string) => {
-    router.replace(`${pathname}?${boardGameNameKey}=${value}`);
-  }, 300);
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    router.replace(`${pathname}?${boardGameNameKey}=${e.target.value}`);
+  };
+
+  const debouncedOnChange = useDebouncedCallback(handleChange, 300);
 
   return (
     <Command className={"w-72 sm:w-96"}>
       <CommandInput
-        placeholder="Search..."
         defaultValue={searchParams.get(boardGameNameKey)?.toString()}
-        onValueChange={onChange}
+        onChange={debouncedOnChange}
       />
       {children}
     </Command>
