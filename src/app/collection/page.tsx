311f843f5b13f841b963c99115cda28a4e195b95
Stan <stephenppercival@gmail.com>
2024-07-25 21:37:16 +0100

    Collection styling

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -12,12 +12,42 @@ export default async function Collection() {
 
   return (
     <>
-      <h1>Collection, you&apos;re logged in!</h1>
-      <ul>
-        {collection.map((bg) => (
-          <li key={bg.id}>{bg.name}</li>
-        ))}
-      </ul>
+      <h1 className="mb-4 text-2xl font-bold">
+        Collection, you&apos;re logged in!
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="min-w-full rounded-lg bg-white text-black shadow-md">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border-b-2 border-gray-300 px-4 py-2">ID</th>
+              <th className="border-b-2 border-gray-300 px-4 py-2">Name</th>
+              <th colSpan={2} className="border-b-2 border-gray-300 px-4 py-2">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {collection.map((bg) => (
+              <tr key={bg.id} className="even:bg-gray-100">
+                <td className="border-b border-gray-300 px-4 py-2">{bg.id}</td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {bg.name}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  <button className="text-red-500 hover:text-red-700">
+                    Delete
+                  </button>
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  <button className="text-blue-500 hover:text-blue-700">
+                    Add Play
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
