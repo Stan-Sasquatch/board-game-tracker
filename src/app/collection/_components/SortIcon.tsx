00b485c00f121c76b2aafc32497791dfc6803d2e
Stan <stephenppercival@gmail.com>
2024-08-19 21:05:34 +0100

    disable sorting during transition

--- src/app/collection/_components/SortIcon.tsx
+++ src/app/collection/_components/SortIcon.tsx
@@ -7,51 +7,78 @@ import {
 } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { setUrlParams } from "~/app/search/utils";
+import { useTransition } from "react";
 
 export function SortIcon({
   orderBy,
   direction,
   field,
-  color,
+  colours,
 }: {
   orderBy: "name" | "playCount" | undefined;
   direction: "asc" | "desc" | undefined;
   field: "name" | "playCount";
-  color?: string;
+  colours: { pending: string; primary: string };
 }) {
   const pathname = usePathname();
   const router = useRouter();
   const params = new URLSearchParams();
+  const [isPending, startTransition] = useTransition();
 
+  const calculatedColour = isPending ? colours.pending : colours.primary;
   function PushQuery() {
     router.replace(`${pathname}?${params.toString()}`);
   }
 
   if (orderBy !== field || !direction) {
     function onUpDownClick() {
-      setUrlParams(params, "orderBy", field);
-      setUrlParams(params, "direction", "asc");
-      PushQuery();
+      if (isPending) {
+        return;
+      }
+
+      startTransition(() => {
+        setUrlParams(params, "orderBy", field);
+        setUrlParams(params, "direction", "asc");
+        PushQuery();
+      });
     }
 
-    return <ArrowUpDown onClick={onUpDownClick} color={color} />;
+    return (
+      <ArrowUpDown
+        onClick={onUpDownClick}
+        color={calculatedColour}
+        aria-disabled={isPending}
+      />
+    );
   }
 
   if (direction === "asc") {
     function onUpClick() {
-      setUrlParams(params, "orderBy", field);
-      setUrlParams(params, "direction", "desc");
-      PushQuery();
+      if (isPending) {
+        return;
+      }
+
+      startTransition(() => {
+        setUrlParams(params, "orderBy", field);
+        setUrlParams(params, "direction", "desc");
+        PushQuery();
+      });
     }
 
-    return <ArrowUpWideNarrow onClick={onUpClick} color={color} />;
+    return <ArrowUpWideNarrow onClick={onUpClick} color={calculatedColour} />;
   }
 
   function onDownClick() {
-    params.delete("orderBy");
-    params.delete("direction");
-    PushQuery();
+    if (isPending) {
+      return;
+    }
+
+    startTransition(() => {
+      params.delete("orderBy");
+      params.delete("direction");
+      PushQuery();
+    });
   }
 
-  return <ArrowDownWideNarrow onClick={onDownClick} color={color} />;
+  return <ArrowDownWideNarrow onClick={onDownClick} color={calculatedColour} />;
 }
